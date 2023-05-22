32c6d72c0e062e1aacc3adda439a0f42f22ff2ed
KevinSneyderH <kevin.sneyder.hernandez@gmail.com>
2023-05-22 11:15:23 -0500

    Se agregio un metodo al Main.java

--- Main.java
+++ Main.java
@@ -2,3 +2,12 @@ public class main {
 private int numero;
 private String cadena;
 }
+
+public void cadenasTexto(){
+String cadena1 = null;
+
+for(int i = 0, i < 5, i++){
+System.out.print("Hola Mundo");
+}
+
+}
